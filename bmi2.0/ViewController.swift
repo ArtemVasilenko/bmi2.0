44f38e3fafcfb767ded4d3907680c799cf41b963
ArtemVasilenko <artemvasilenko88@gmail.com>
2019-01-10 21:25:44 +0200

    Initial Commit

---
+++ bmi2.0/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  bmi2.0
+//
+//  Created by Артем on 1/10/19.
+//  Copyright © 2019 Артем. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
