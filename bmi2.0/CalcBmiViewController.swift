7d37a1fbb8518d4d9c6482e0c0b88f48e4ff0d72
ArtemVasilenko <artemvasilenko88@gmail.com>
2019-01-21 13:28:24 +0200

    Add scrollView

--- bmi2.0/CalcBmiViewController.swift
+++ bmi2.0/CalcBmiViewController.swift
@@ -2,38 +2,24 @@ import UIKit
 
 class CalcBmiViewController: UIViewController {
     var bmi: Bmi = BmiCalc()
-    var resultImage = UIImageView()
-    @IBOutlet weak var labelResult: UILabel!
     
-
-    fileprivate func createImageView() {
-        self.resultImage.frame = CGRect(x: self.view.frame.size.width/5, y: 80, width: 200, height: 200)
-        self.resultImage.contentMode = .scaleAspectFit
-        self.resultImage.isUserInteractionEnabled = true
-        self.view.addSubview(resultImage)
-    }
+    @IBOutlet weak var resultImage: UIImageView!
+    @IBOutlet weak var labelResult: UILabel!
+    @IBOutlet weak var scrollView: UIScrollView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        //MARK: TapUser
-        let tapUser = UITapGestureRecognizer(target: self, action: #selector (userTap(tap:)))
-        resultImage.isUserInteractionEnabled = true
-        resultImage.addGestureRecognizer(tapUser)
-        
-        
-        
-        //MARK: create Image and label
-        createImageView()
+        self.scrollView.delegate = self
+        self.scrollView.maximumZoomScale = 6
+        self.scrollView.minimumZoomScale = 1.0
+        //MARK: calculation results into label and image
         labelResult.text = bmi.result.0
         resultImage.image = bmi.result.1
     }
-    
-    @objc func userTap (tap: Any) {
-        UIView.animate(withDuration: 0.75, animations: {
-            self.resultImage.center.y += 10
-        })
-        
+}
+
+extension CalcBmiViewController: UIScrollViewDelegate {
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return resultImage.self
     }
-    
 }
